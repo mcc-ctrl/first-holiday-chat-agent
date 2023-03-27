fbf2a5a44744055e45515a17a43e9605d7b5f291
Matthew Mccormack <Matt@Matthews-MacBook-Pro.local>
2023-03-27 15:16:33 +0100

    data base and api created

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    db: {
+        /* don't expose password or any sensitive info, done only for demo */
+        host: "localhost",
+        user: "Matt",
+        password: "Projectx=23",
+        database: "holiday_packages",
+    },
+    listPerPage: '',
+};
+module.exports = config;
